9c9f53e73174551f4259d61a4a5518b1e53c4daf
umetnic <30912764+umetnic@users.noreply.github.com>
2023-09-20 21:23:44 +0200

    working

--- convert_eac3_to_ac3_TEST.sh
+++ convert_eac3_to_ac3_TEST.sh
@@ -16,6 +16,7 @@ log_file="$output_folder/converted_files.log"
 skipped_log_file="$output_folder/skipped_files.log"
 # These lines set variables for the output directories and the log files location.
 
+ echo -e "\n" # Just a new line to make things clearer to see
 
 # Function to convert an MKV file
 convert_mkv_file() {
@@ -49,10 +50,11 @@ convert_mkv_file() {
                 # Check if the output file already exists before running ffmpeg
                 if [ ! -f "$output_file" ]; then
                     # Extract the audio file using FFmpeg without overwriting
-                    #ffmpeg -loglevel debug -n -i "$input_file" -vn -c:a ac3 -ac 6 -ar 48000 "$output_file"
-                    ffmpeg -loglevel debug -n -i "$input_file" -vn -c:a ac3 -ac 6 -ar 48000 "$output_file" 2>&1 | grep -E 'Opening an input file:|Stream.* (Video|Audio)'
-                    #ffmpeg -loglevel debug -n -i "$input_file" -vn -c:a ac3 -ac 6 -ar 48000 "$output_file" 2>&1 | grep -E 'Input file #0|Output file #0|Stream #0|Output #0'
-
+                    echo -e "\n"                    
+					echo -e "\e[32mProcessing:\e[0m"
+					ffmpeg -loglevel info -n -i "$input_file" -vn -c:a ac3 -ac 6 -ar 48000 "$output_file" 2>&1 | grep -E 'Input #0|Stream.* (Audio)|Output file #0 ->'
+                    
+					
                     # Add the converted file name to the list
                     converted_files+=("$filename")
 
@@ -63,16 +65,17 @@ convert_mkv_file() {
                     echo "$filename" >> "$log_file"
 
                     # Print information about the audio stream to the terminal in yellow
-                    echo -e "\e[33mAudio stream info for $filename:\e[0m"
-                    echo -e "\e[33m$audio_info\e[0m"
-                    echo ""
+                    #echo -e "\e[33mAudio stream info for $filename:\e[0m"
+                    #echo -e "\e[33m$audio_info\e[0m"
+                    #echo ""
 
                     # Increment the newly converted files count
                     newly_converted_files=$((newly_converted_files + 1))
                 else
                     
                     # Print a message indicating that the file already exists and won't be overwritten
-                    echo "File already exists, skipping: $output_file"
+                    echo -e "Skipping existing file:\e[96m\e[1m$output_file\e[0m"
+                    
                     # Log the skipped file in the skipped_files.log
                     echo "Skipped file (already exists): $filename" >> "$skipped_log_file"					
 					
@@ -133,10 +136,12 @@ do
     # Notify if no new files were converted
     if [[ $newly_converted_files -eq 0 ]]; then
         # Print in green color
+		echo -e "\n"
         echo -e "\e[32mNo new files to convert.\e[0m"
         # You can also add additional notification methods here, like sending an email or using a system notification.
     else
         # Print in green color
+		echo -e "\n"
         echo -e "\e[32mConverted files on this run:\e[0m"
         for converted_filename in "${files_converted_this_run[@]}"; do
             # Print in yellow color
